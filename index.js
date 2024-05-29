34b68aea77df27cf2c0142c27dbcbf9e242d71f5
pheathtwilio <pheath@twilio.com>
2024-05-29 15:12:10 -0500

    added locale summary

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const getCountriesFromURL = async (url) => {
 
         const guidelineCountryList = '#guidelineCountryList > div > div.grid-container.three-columns.default.medium-gap'
 
-        console.log($(guidelineCountryList).children().length)
-
         $(guidelineCountryList).children().each((index, element) => {
 
             var country = {
@@ -30,4 +28,31 @@ const getCountriesFromURL = async (url) => {
     }
 }
 
+const getLocaleSummary = async (url) => {
+
+    try{
+
+        const { data } = await axios.get(url)
+        const $ = cheerio.load(data)
+
+        const localeSummaryTable = '#guideline-tables > div > div:nth-child(1) > div > div > table > tbody'
+        const localeSummary = []
+
+        $(localeSummaryTable).find('tr').each((index, row) => {
+
+            localeSummary.push({
+                [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+            })
+
+        })
+
+        return localeSummary
+
+    }catch (error) {
+        console.error(`Error: ${error}`)
+    }
+}
+
 const countries = getCountriesFromURL('https://www.twilio.com/en-us/guidelines/voice')
+
+const localeSummary = getLocaleSummary('https://www.twilio.com/en-us/guidelines/ar/voice')
