4173f4210831c79f9e901891a28ee06f01af7460
pheathtwilio <pheath@twilio.com>
2024-05-29 15:37:16 -0500

    added parsing of callerId, DTMF and Emergency Calling

--- index.js
+++ index.js
@@ -78,8 +78,97 @@ const getReachability = async (url) => {
     }
 }
 
+const getCallerID = async (url) => {
+
+    try{
+
+        const { data } = await axios.get(url)
+        const $ = cheerio.load(data)
+
+        const callerTable = '#guideline-tables > div > div:nth-child(3) > div > div > table > tbody'
+        const callerids = []
+
+        $(callerTable).find('tr').each((index, row) => {
+
+            callerids.push({
+                [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+            })
+
+        })
+
+        console.log(callerids)
+
+        return callerids
+
+    }catch (error) {
+        console.error(`Error: ${error}`)
+    }
+}
+
+const getDTMF = async (url) => {
+
+    try{
+
+        const { data } = await axios.get(url)
+        const $ = cheerio.load(data)
+
+        const dtmfTable = '#guideline-tables > div > div:nth-child(4) > div > div > table > tbody'
+        const dtmfs = []
+
+        $(dtmfTable).find('tr').each((index, row) => {
+
+            dtmfs.push({
+                [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+            })
+
+        })
+
+        console.log(dtmfs)
+
+        return dtmfs
+
+    }catch (error) {
+        console.error(`Error: ${error}`)
+    }
+}
+
+const getEmergencyCalling = async (url) => {
+
+    try{
+
+        const { data } = await axios.get(url)
+        const $ = cheerio.load(data)
+
+        const emergencyCallingTable = '#guideline-tables > div > div:nth-child(5) > div > div > table > tbody'
+        const emergencies = []
+
+        $(emergencyCallingTable).find('tr').each((index, row) => {
+
+            emergencies.push({
+                [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+            })
+
+        })
+
+        console.log(emergencies)
+
+        return emergencies
+
+    }catch (error) {
+        console.error(`Error: ${error}`)
+    }
+}
+
+
+
 const countries = getCountriesFromURL('https://www.twilio.com/en-us/guidelines/voice')
 
 const localeSummary = getLocaleSummary('https://www.twilio.com/en-us/guidelines/ar/voice')
 
 const reachability = getReachability('https://www.twilio.com/en-us/guidelines/ar/voice')
+
+const callerids = getCallerID('https://www.twilio.com/en-us/guidelines/ar/voice')
+
+const dtmfs = getDTMF('https://www.twilio.com/en-us/guidelines/ar/voice')
+
+const emergencies = getEmergencyCalling('https://www.twilio.com/en-us/guidelines/ar/voice')
